acc4da5da1d11425aa00e93d35e2aba2b4bd9a4a
ryotatanks9981 <67944448+ryotatanks9981@users.noreply.github.com>
2021-11-28 20:15:47 +0900

    make template

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,10 @@
+<header>
+    <form action="" method="get">
+    <ul>
+        <li><img src="{{asset("img/logo.svg")}}" alt="logo"></li>
+        <li><h1><a href="/">Swift Manual</a></h1></li>
+        <li><input type="text" name="query" placeholder="関数" id="textview"></li>
+        <li><input type="submit" value="検索"></li>
+    </ul>
+</form>
+</header>
